198cf5d2836f3db8a0f3fab59fd43a97c39aa859
WilliamZeng1115 <WilliamZeng1115@users.noreply.github.com>
2023-04-30 08:31:48 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,13 +7,13 @@ def hello():
     """
     Print out Hello World in a function.
     """
-    print("Hello World in a function.")
+    print("Hello World !!!!!!!.")
 
 # Call a function that prints out Hello World
 hello()
 
 # Print out Hello World 
-print("Just saying Hello World")
+print("Hello World")
 
 # Assign the string Hello World to a variable and print it out
 str = "Hello World in a variable"
